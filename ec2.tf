691d6256effce6760f54d7f68d2664807e40db82
Shweta Kumari <69496909+shweta-github04@users.noreply.github.com>
2021-06-10 17:42:33 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -6,29 +6,12 @@ provider "aws" {
 
 #................latest ubuntu AMI.............
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-#.............ubuntu server.................
-
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
+  acl    = "private"
 
   tags = {
-    Name = "HelloWorld"
+    Name        = "My bucket"
+    Environment = "Dev"
   }
 }
