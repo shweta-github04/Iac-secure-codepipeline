99038bec5810d65a56da0f789ce73f5b794778b0
Shweta Kumari <69496909+shweta-github04@users.noreply.github.com>
2021-06-10 18:35:10 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -8,6 +8,14 @@ provider "aws" {
 resource "aws_s3_bucket" "log_bucket" {
   bucket = "my-tf-log-bucket"
   acl    = "log-delivery-write"
+  
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  } 
 }
 
 resource "aws_s3_bucket" "test-tf-enc" {
