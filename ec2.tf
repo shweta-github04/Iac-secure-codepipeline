c3a1ee383b5e88ea6a93e29337491ec34ce7d7f7
Shweta Kumari <69496909+shweta-github04@users.noreply.github.com>
2021-06-10 18:32:48 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -5,12 +5,19 @@ provider "aws" {
 }
 
 #................latest ubuntu AMI.............
+resource "aws_s3_bucket" "log_bucket" {
+  bucket = "my-tf-log-bucket"
+  acl    = "log-delivery-write"
+}
 
 resource "aws_s3_bucket" "test-tf-enc" {
   bucket = "test-tf-enc"
   acl    = "private"
 
-
+  logging {
+  target_bucket = aws_s3_bucket.log_bucket.id
+  target_prefix = "log/"
+  
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
@@ -19,3 +26,5 @@ resource "aws_s3_bucket" "test-tf-enc" {
     }
   }
 }
+
+
