66412cd55fdc7ef6fd66b45f602a17999e5754cb
Shweta Kumari <69496909+shweta-github04@users.noreply.github.com>
2021-06-10 18:00:11 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -8,7 +8,13 @@ provider "aws" {
 
 resource "aws_s3_bucket" "b" {
   bucket = "192.16.134.22."
-  acl    = "none"
+  acl    = "private"
+  
+  logging {
+  target_bucket = aws_s3_bucket.log_bucket.id
+  target_prefix = "log/"
+  }
+}
 
   tags = {
     Name        = "My bucket"
