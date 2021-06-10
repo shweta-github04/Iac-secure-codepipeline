108fc721420086d80a653dc7146ddc8b497b42cc
Shweta Kumari <69496909+shweta-github04@users.noreply.github.com>
2021-06-10 18:11:41 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -9,16 +9,21 @@ provider "aws" {
 resource "aws_s3_bucket" "log_bucket" {
   bucket = "my-tf-log-bucket"
   acl    = "log-delivery-write"
-  server_side_encryption = "AES256"
 }
 
 resource "aws_s3_bucket" "b" {
   bucket = "my-tf-test-bucket"
   acl    = "private"
-  server_side_encryption = "AES256"
 
   logging {
     target_bucket = aws_s3_bucket.log_bucket.id
     target_prefix = "log/"
   }
+}
+
+resource "aws_s3_bucket_object" "examplebucket_object" {
+  key                    = "someobject"
+  bucket                 = aws_s3_bucket.b.id
+  source                 = "index.html"
+  server_side_encryption = "AES256"
 }
