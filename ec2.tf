812433578fd2d0076a75e8887d088ffbf8e46919
Shweta <69496909+shweta-github04@users.noreply.github.com>
2021-06-14 15:47:30 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,11 @@
+
+#............. Provider.................
+
+provider "aws" {
+  region  = "us-east-1"
+}
+#..............................
+
 resource "aws_vpc" "my_vpc" {
   cidr_block = "172.16.0.0/16"
 
