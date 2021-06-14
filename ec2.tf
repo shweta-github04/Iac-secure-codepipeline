dc0640202784c0c17b8ec8a73608c0328a8b1321
Shweta <69496909+shweta-github04@users.noreply.github.com>
2021-06-14 21:18:34 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -112,11 +112,35 @@ resource "aws_kms_key" "a" {
   deletion_window_in_days = 10
   is_enabled              = true
   enable_key_rotation     = true
+  policy = <<EOF
+{
+  "Version" : "2012-10-17",
+  "Id" : "key-default-1",
+  "Statement" : [ {
+      "Sid" : "Enable IAM User Permissions",
+      "Effect" : "Allow",
+      "Principal" : {
+        "AWS" : "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+      },
+      "Action" : "kms:*",
+      "Resource" : "*"
+    },
+    {
+      "Effect": "Allow",
+      "Principal": { "Service": "logs.${var.region}.amazonaws.com" },
+      "Action": [ 
+        "kms:Encrypt*",
+        "kms:Decrypt*",
+        "kms:ReEncrypt*",
+        "kms:GenerateDataKey*",
+        "kms:Describe*"
+      ],
+      "Resource": "*"
+    }  
+  ]
 }
+EOF
 
-resource "aws_kms_alias" "key_alias" {
-  name          = "alias/test"
-  target_key_id = aws_kms_key.a.id
 }
 
 resource "aws_cloudwatch_log_group" "flow_log_group" {
