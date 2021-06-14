514e4bddb636c18b220f37af2cca7858ac90273b
Shweta <69496909+shweta-github04@users.noreply.github.com>
2021-06-14 12:37:51 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -24,35 +24,6 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-#............Network Interface............
-
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "172.16.0.0/16"
-
-  tags = {
-    Name = "tf-example"
-  }
-}
-
-resource "aws_subnet" "my_subnet" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "172.16.10.0/24"
-  availability_zone = "us-west-2a"
-
-  tags = {
-    Name = "tf-example"
-  }
-}
-
-resource "aws_network_interface" "network_interface_ok" {
-  subnet_id   = aws_subnet.my_subnet.id
-  private_ips = ["172.16.10.100"]
-
-  tags = {
-    Name = "primary_network_interface"
-  }
-}
-
 #.............ubuntu server.................
 
 resource "aws_instance" "web" {
