2dbd665221845bffc78a900d3686e1dca1aa4419
Shweta <69496909+shweta-github04@users.noreply.github.com>
2021-06-14 13:46:47 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -32,7 +32,11 @@ resource "aws_instance" "web" {
   monitoring        = true
   instance_type     = "t3.micro"
   vpc_security_group_ids = ["sg-0676242cf7930910d"]
-  subnet_id              = "subnet-053cc94194e1b9125"
+  
+ network_interface {
+    network_interface_id = "subnet-053cc94194e1b9125"
+    device_index         = 0
+  }
   
   metadata_options {
      http_endpoint = "enabled"
