06517b1d8bb4f3e525226f4c40745b9dacd288b6
Shweta <69496909+shweta-github04@users.noreply.github.com>
2021-06-15 20:45:08 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -89,13 +89,6 @@ resource "aws_security_group" "allow_tls" {
 
 resource "aws_default_security_group" "default" {
   vpc_id = aws_vpc.my_vpc.id
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
 
 resource "aws_instance" "web" {
