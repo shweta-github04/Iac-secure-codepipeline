37de19c9937080308e20c02c3b32f34679811368
Shweta <69496909+shweta-github04@users.noreply.github.com>
2021-06-15 21:16:41 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -68,8 +68,7 @@ resource "aws_security_group" "allow_tls" {
     from_port        = 443
     to_port          = 443
     protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.my_vpc.cidr_block]
-    ipv6_cidr_blocks = [aws_vpc.my_vpc.ipv6_cidr_block]
+    cidr_blocks      = ["10.0.0.0/0"]
   }
 
   egress {
