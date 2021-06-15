716c851d80815f94f3637975134a7f08d563cdd2
Shweta <69496909+shweta-github04@users.noreply.github.com>
2021-06-15 20:30:00 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -89,7 +89,7 @@ resource "aws_instance" "web" {
   ami               = data.aws_ami.ubuntu.id
   ebs_optimized     = true
   monitoring        = true
-  instance_type     = "t3.micro"security_groups = [aws_security_group.ok_sg.id]
+  instance_type     = "t3.micro"
   security_groups = [aws_security_group.allow_tls.id]
 
   network_interface {
