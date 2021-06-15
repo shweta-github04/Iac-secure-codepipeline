9466f97ba489ab0c197db9b2aca1325f400d355a
Shweta <69496909+shweta-github04@users.noreply.github.com>
2021-06-15 20:25:34 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -56,8 +56,33 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_default_security_group" "default" {
-  vpc_id = aws_vpc.my_vpc.id
+#..........Security group............
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.my_vpc.cidr_block]
+    ipv6_cidr_blocks = [aws_vpc.my_vpc.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
 }
 
 resource "aws_instance" "web" {
