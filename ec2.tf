5d808eb837e9869b5d57b1abd293619baf66f37c
Shweta <69496909+shweta-github04@users.noreply.github.com>
2021-06-15 21:24:44 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -56,34 +56,6 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-#..........Security group............
-
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.my_vpc.id
-
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "allow_tls"
-  }
-}
-
 #..........default Security group.........
 
 resource "aws_default_security_group" "default" {
@@ -95,7 +67,6 @@ resource "aws_instance" "web" {
   ebs_optimized     = true
   monitoring        = true
   instance_type     = "t3.micro"
-  security_groups = [aws_security_group.allow_tls.id]
 
   network_interface {
     network_interface_id = aws_network_interface.network_interface_ok.id
